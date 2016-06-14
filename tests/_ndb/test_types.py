05e87d47c5f7ad286159aaa72b21071e1508eaa4
Eran Kampf <eran@ekampf.com>
2016-06-13 18:27:02 -0700

    lint

--- tests/_ndb/test_types.py
+++ tests/_ndb/test_types.py
@@ -122,7 +122,7 @@ class TestNDBTypes(BaseTest):
                     return Article.query()
 
             schema = graphene.Schema(query=QueryType)
-            result = schema.execute('query test {  articles { prop } }')
+            schema.execute('query test {  articles { prop } }')
 
         self.assertIn("Model 'bar' is not accessible by the schema.", str(context.exception.message))
 
