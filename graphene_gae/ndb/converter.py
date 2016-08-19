30bc1e391b21aeea09988bec92581c5ecbc22293
Eran Kampf <eran@ekampf.com>
2016-08-19 10:56:49 -0700

    Fixed convertion process for simple scalar fields

--- graphene_gae/ndb/converter.py
+++ graphene_gae/ndb/converter.py
@@ -3,16 +3,12 @@ from collections import namedtuple
 import inflect
 from google.appengine.ext import ndb
 
-from graphene import LazyType
-from graphene.core.types import Field
-from graphene.core.types.definitions import List
-from graphene.core.types.scalars import String, Boolean, Int, Float
-from graphene.core.types.custom_scalars import JSONString, DateTime
-from graphene_gae.ndb.fields import NdbKeyField, NdbKeyStringField
+from graphene import String, Boolean, Int, Float, List, NonNull, Field
+from graphene.types.json import JSONString
+from graphene.types.datetime import DateTime
+# from graphene_gae.ndb.fields import NdbKeyField, NdbKeyStringField
 
 __author__ = 'ekampf'
-
-ConversionResult = namedtuple('ConversionResult', ['name', 'field'])
 
 p = inflect.engine()
 
@@ -26,91 +22,88 @@ def convert_ndb_scalar_property(graphene_type, ndb_prop, **kwargs):
     description = "%s %s property" % (ndb_prop._name, graphene_type)
     result = graphene_type(description=description, **kwargs)
     if ndb_prop._repeated:
-        result = result.List
+        result = List(result)
 
     if ndb_prop._required:
-        result = result.NonNull
+        result = NonNull(result)
 
     return result
 
 
-def convert_ndb_string_property(ndb_prop, meta):
+def convert_ndb_string_property(ndb_prop):
     return convert_ndb_scalar_property(String, ndb_prop)
 
 
-def convert_ndb_boolean_property(ndb_prop, meta):
+def convert_ndb_boolean_property(ndb_prop):
     return convert_ndb_scalar_property(Boolean, ndb_prop)
 
 
-def convert_ndb_int_property(ndb_prop, meta):
+def convert_ndb_int_property(ndb_prop):
     return convert_ndb_scalar_property(Int, ndb_prop)
 
 
-def convert_ndb_float_property(ndb_prop, meta):
+def convert_ndb_float_property(ndb_prop):
     return convert_ndb_scalar_property(Float, ndb_prop)
 
 
-def convert_ndb_json_property(ndb_prop, meta):
-    return JSONString()
+def convert_ndb_json_property(ndb_prop):
+    return JSONString(description=ndb_prop._name)
 
 
-def convert_ndb_datetime_property(ndb_prop, meta):
-    return DateTime()
+def convert_ndb_datetime_property(ndb_prop):
+    return DateTime(description=ndb_prop._name)
 
 
-def convert_ndb_key_propety(ndb_key_prop, meta):
-    """
-    Two conventions for handling KeyProperties:
-    #1.
-        Given:
-            store_key = ndb.KeyProperty(...)
+# def convert_ndb_key_propety(ndb_key_prop):
+#     """
+#     Two conventions for handling KeyProperties:
+#     #1.
+#         Given:
+#             store_key = ndb.KeyProperty(...)
+#
+#         Result is 2 fields:
+#             store_id  = graphene.String() -> resolves to store_key.urlsafe()
+#             store     = NdbKeyField()     -> resolves to entity
+#
+#     #2.
+#         Given:
+#             store = ndb.KeyProperty(...)
+#
+#         Result is 2 fields:
+#             store_id = graphene.String() -> resolves to store_key.urlsafe()
+#             store     = NdbKeyField()     -> resolves to entity
+#
+#     """
+#     name = ndb_key_prop._code_name
+#
+#     if name.endswith('_key') or name.endswith('_keys'):
+#         # Case #1 - name is of form 'store_key' or 'store_keys'
+#         string_prop_name = rreplace(name, '_key', '_id', 1)
+#         resolved_prop_name = name[:-4] if name.endswith('_key') else p.plural(name[:-5])
+#     else:
+#         # Case #2 - name is of form 'store'
+#         singular_name = p.singular_noun(name) if p.singular_noun(name) else name
+#         string_prop_name = singular_name + '_ids' if ndb_key_prop._repeated else singular_name + '_id'
+#         resolved_prop_name = name
+#
+#     string_field = NdbKeyStringField(name, ndb_key_prop._kind)
+#     resolved_field = NdbKeyField(name, ndb_key_prop._kind)
+#
+#     if ndb_key_prop._repeated:
+#         string_field = string_field.List
+#         resolved_field = resolved_field.List
+#
+#     if ndb_key_prop._required:
+#         string_field = string_field.NonNull
+#         resolved_field = resolved_field.NonNull
+#
+#     return [
+#         string_field,
+#         resolved_field
+#     ]
+#
 
-        Result is 2 fields:
-            store_id  = graphene.String() -> resolves to store_key.urlsafe()
-            store     = NdbKeyField()     -> resolves to entity
-
-    #2.
-        Given:
-            store = ndb.KeyProperty(...)
-
-        Result is 2 fields:
-            store_id = graphene.String() -> resolves to store_key.urlsafe()
-            store     = NdbKeyField()     -> resolves to entity
-
-    """
-    name = ndb_key_prop._code_name
-
-    if name.endswith('_key') or name.endswith('_keys'):
-        # Case #1 - name is of form 'store_key' or 'store_keys'
-        string_prop_name = rreplace(name, '_key', '_id', 1)
-        resolved_prop_name = name[:-4] if name.endswith('_key') else p.plural(name[:-5])
-    else:
-        # Case #2 - name is of form 'store'
-        singular_name = p.singular_noun(name) if p.singular_noun(name) else name
-        string_prop_name = singular_name + '_ids' if ndb_key_prop._repeated else singular_name + '_id'
-        resolved_prop_name = name
-
-    string_field = NdbKeyStringField(name, ndb_key_prop._kind)
-    resolved_field = NdbKeyField(name, ndb_key_prop._kind)
-
-    if ndb_key_prop._repeated:
-        string_field = string_field.List
-        resolved_field = resolved_field.List
-
-    if ndb_key_prop._required:
-        string_field = string_field.NonNull
-        resolved_field = resolved_field.NonNull
-
-    string_key_field_result = ConversionResult(name=string_prop_name, field=string_field)
-    resolve_key_field_result = ConversionResult(name=resolved_prop_name, field=resolved_field)
-
-    return [
-        string_key_field_result,
-        resolve_key_field_result
-    ]
-
-
-def convert_local_structured_property(ndb_structured_prop, meta):
+def convert_local_structured_property(ndb_structured_prop):
     is_required = ndb_structured_prop._required
     is_repeated = ndb_structured_prop._repeated
     model = ndb_structured_prop._modelclass
@@ -124,7 +117,7 @@ def convert_local_structured_property(ndb_structured_prop, meta):
     return ConversionResult(name=name, field=Field(t))
 
 
-def convert_computed_property(ndb_computed_prop, meta):
+def convert_computed_property(ndb_computed_prop):
     return convert_ndb_scalar_property(String, ndb_computed_prop)
 
 
@@ -137,22 +130,19 @@ converters = {
     ndb.JsonProperty: convert_ndb_json_property,
     ndb.DateProperty: convert_ndb_datetime_property,
     ndb.DateTimeProperty: convert_ndb_datetime_property,
-    ndb.KeyProperty: convert_ndb_key_propety,
+    # ndb.KeyProperty: convert_ndb_key_propety,
     ndb.LocalStructuredProperty: convert_local_structured_property,
     ndb.ComputedProperty: convert_computed_property
 }
 
 
-def convert_ndb_property(prop, meta=None):
+def convert_ndb_property(prop):
     converter_func = converters.get(type(prop))
     if not converter_func:
         raise Exception("Don't know how to convert NDB field %s (%s)" % (prop._code_name, prop))
 
-    result = converter_func(prop, meta)
-    if not result:
-        raise Exception("Failed to convert NDB field %s (%s)" % (prop._code_name, prop))
+    field = converter_func(prop)
+    if not field:
+        raise Exception("Failed to convert NDB propeerty to a GraphQL field %s (%s)" % (prop._code_name, prop))
 
-    if isinstance(result, (list, ConversionResult,)):
-        return result
-
-    return ConversionResult(name=prop._code_name, field=result)
+    return field
--- tests/_ndb/test_converter.py
+++ tests/_ndb/test_converter.py
@@ -4,10 +4,11 @@ from tests.base_test import BaseTest
 from google.appengine.ext import ndb
 
 import graphene
-from graphene.core.types.custom_scalars import DateTime, JSONString
-from graphene.core.types.definitions import List, NonNull
+from graphene import List, NonNull
+from graphene.types.json import JSONString
+from graphene.types.datetime import DateTime
 
-from graphene_gae.ndb.fields import NdbKeyStringField, NdbKeyField
+# from graphene_gae.ndb.fields import NdbKeyStringField, NdbKeyField
 from graphene_gae.ndb.converter import convert_ndb_property
 
 __author__ = 'ekampf'
@@ -20,8 +21,7 @@ class SomeWeirdUnknownProperty(ndb.Property):
 class TestNDBConverter(BaseTest):
     def __assert_conversion(self, ndb_property_type, expected_graphene_type, *args, **kwargs):
         ndb_property = ndb_property_type(*args, **kwargs)
-        conversion_result = convert_ndb_property(ndb_property)
-        graphene_field_type = conversion_result.field
+        graphene_field_type = convert_ndb_property(ndb_property)
         self.assertIsInstance(graphene_field_type, expected_graphene_type)
 
     def testUnknownProperty_raisesException(self):
@@ -35,23 +35,30 @@ class TestNDBConverter(BaseTest):
     @mock.patch('graphene_gae.ndb.converter.converters')
     def testNoneResult_raisesException(self, patch_convert):
         from graphene_gae.ndb.converter import convert_ndb_property
-        patch_convert.get.return_value = lambda _, __: None
+        patch_convert.get.return_value = lambda _: None
         with self.assertRaises(Exception) as context:
             prop = ndb.StringProperty()
             prop._code_name = "my_prop"
             convert_ndb_property(prop)
 
-        self.assertTrue("Failed to convert NDB field my_prop" in context.exception.message, msg=context.exception.message)
+        expected_message = 'Failed to convert NDB propeerty to a GraphQL field my_prop (StringProperty())'
+        self.assertTrue(expected_message in context.exception.message, msg=context.exception.message)
 
     def testStringProperty_shouldConvertToString(self):
         self.__assert_conversion(ndb.StringProperty, graphene.String)
 
     def testStringProperty_repeated_shouldConvertToList(self):
         ndb_prop = ndb.StringProperty(repeated=True)
-        conversion_result = convert_ndb_property(ndb_prop)
-        graphene_type = conversion_result.field
+        graphene_type = convert_ndb_property(ndb_prop)
 
         self.assertIsInstance(graphene_type, graphene.List)
+        self.assertIsInstance(graphene_type.of_type, graphene.String)
+
+    def testStringProperty_required_shouldConvertToList(self):
+        ndb_prop = ndb.StringProperty(required=True)
+        graphene_type = convert_ndb_property(ndb_prop)
+
+        self.assertIsInstance(graphene_type, graphene.NonNull)
         self.assertIsInstance(graphene_type.of_type, graphene.String)
 
     def testTextProperty_shouldConvertToString(self):
@@ -75,62 +82,62 @@ class TestNDBConverter(BaseTest):
     def testJsonProperty_shouldConvertToString(self):
         self.__assert_conversion(ndb.JsonProperty, JSONString)
 
-    def testKeyProperty_withSuffix(self):
-        prop = ndb.KeyProperty()
-        prop._code_name = 'user_key'
-
-        conversion = convert_ndb_property(prop)
-
-        self.assertLength(conversion, 2)
-
-        self.assertEqual(conversion[0].name, 'user_id')
-        self.assertIsInstance(conversion[0].field, NdbKeyStringField)
-
-        self.assertEqual(conversion[1].name, 'user')
-        self.assertIsInstance(conversion[1].field, NdbKeyField)
-
-    def testKeyProperty_withSuffix_repeated(self):
-        prop = ndb.KeyProperty(repeated=True)
-        prop._code_name = 'user_keys'
-
-        conversion = convert_ndb_property(prop)
-
-        self.assertLength(conversion, 2)
-
-        self.assertEqual(conversion[0].name, 'user_ids')
-        self.assertIsInstance(conversion[0].field, List)
-        self.assertIsInstance(conversion[0].field.of_type, NdbKeyStringField)
-
-        self.assertEqual(conversion[1].name, 'users')
-        self.assertIsInstance(conversion[1].field, List)
-        self.assertIsInstance(conversion[1].field.of_type, NdbKeyField)
-
-    def testKeyProperty_withSuffix_required(self):
-        prop = ndb.KeyProperty(required=True)
-        prop._code_name = 'user_key'
-
-        conversion = convert_ndb_property(prop)
-
-        self.assertLength(conversion, 2)
-
-        self.assertEqual(conversion[0].name, 'user_id')
-        self.assertIsInstance(conversion[0].field, NonNull)
-        self.assertIsInstance(conversion[0].field.of_type, NdbKeyStringField)
-
-        self.assertEqual(conversion[1].name, 'user')
-        self.assertIsInstance(conversion[1].field, NonNull)
-        self.assertIsInstance(conversion[1].field.of_type, NdbKeyField)
-
-    def testKeyProperty_withoutSuffix(self):
-        prop = ndb.KeyProperty()
-        prop._code_name = 'user'
-
-        conversion = convert_ndb_property(prop)
-
-        self.assertLength(conversion, 2)
-
-        self.assertEqual(conversion[0].name, 'user_id')
-        self.assertIsInstance(conversion[0].field, NdbKeyStringField)
-
-        self.assertEqual(conversion[1].name, 'user')
-        self.assertIsInstance(conversion[1].field, NdbKeyField)
+    # def testKeyProperty_withSuffix(self):
+    #     prop = ndb.KeyProperty()
+    #     prop._code_name = 'user_key'
+    #
+    #     conversion = convert_ndb_property(prop)
+    #
+    #     self.assertLength(conversion, 2)
+    #
+    #     self.assertEqual(conversion[0].name, 'user_id')
+    #     self.assertIsInstance(conversion[0].field, NdbKeyStringField)
+    #
+    #     self.assertEqual(conversion[1].name, 'user')
+    #     self.assertIsInstance(conversion[1].field, NdbKeyField)
+    #
+    # def testKeyProperty_withSuffix_repeated(self):
+    #     prop = ndb.KeyProperty(repeated=True)
+    #     prop._code_name = 'user_keys'
+    #
+    #     conversion = convert_ndb_property(prop)
+    #
+    #     self.assertLength(conversion, 2)
+    #
+    #     self.assertEqual(conversion[0].name, 'user_ids')
+    #     self.assertIsInstance(conversion[0].field, List)
+    #     self.assertIsInstance(conversion[0].field.of_type, NdbKeyStringField)
+    #
+    #     self.assertEqual(conversion[1].name, 'users')
+    #     self.assertIsInstance(conversion[1].field, List)
+    #     self.assertIsInstance(conversion[1].field.of_type, NdbKeyField)
+    #
+    # def testKeyProperty_withSuffix_required(self):
+    #     prop = ndb.KeyProperty(required=True)
+    #     prop._code_name = 'user_key'
+    #
+    #     conversion = convert_ndb_property(prop)
+    #
+    #     self.assertLength(conversion, 2)
+    #
+    #     self.assertEqual(conversion[0].name, 'user_id')
+    #     self.assertIsInstance(conversion[0].field, NonNull)
+    #     self.assertIsInstance(conversion[0].field.of_type, NdbKeyStringField)
+    #
+    #     self.assertEqual(conversion[1].name, 'user')
+    #     self.assertIsInstance(conversion[1].field, NonNull)
+    #     self.assertIsInstance(conversion[1].field.of_type, NdbKeyField)
+    #
+    # def testKeyProperty_withoutSuffix(self):
+    #     prop = ndb.KeyProperty()
+    #     prop._code_name = 'user'
+    #
+    #     conversion = convert_ndb_property(prop)
+    #
+    #     self.assertLength(conversion, 2)
+    #
+    #     self.assertEqual(conversion[0].name, 'user_id')
+    #     self.assertIsInstance(conversion[0].field, NdbKeyStringField)
+    #
+    #     self.assertEqual(conversion[1].name, 'user')
+    #     self.assertIsInstance(conversion[1].field, NdbKeyField)
